f1ee76710bf6ae983510914e54143972bba48641
Felix <89755606+mystifuzz@users.noreply.github.com>
2023-09-05 16:42:25 +0000

    opis już się nie rusza ten jeden raz

--- tekst.js
+++ tekst.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("animated-text");
+    const opisText = document.querySelector(".opis");
+    const opisPlaceholder = document.querySelector(".opis-placeholder");
     const texts = [
         "I am mysti - some arrogant unicorn!!",
         "jestem mysticzny - taki arogancki jednorożec!!",
@@ -13,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         textElement.textContent = texts[currentIndex];
         setTimeout(function () {
             textElement.style.opacity = "1";
+            opisPlaceholder.style.visibility = "visible";
             setTimeout(fadeOutText, 3000);
         }, 100);
     }
@@ -21,13 +24,15 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!fading) {
             fading = true;
             textElement.style.opacity = "0";
+            opisPlaceholder.style.visibility = "hidden";
             setTimeout(function () {
                 currentIndex = (currentIndex + 1) % texts.length;
                 fadeInText();
+                opisText.style.visibility = "visible";
+                opisText.style.opacity = "1";
             }, 1500);
         }
     }
-
 
     fadeInText();
 });
